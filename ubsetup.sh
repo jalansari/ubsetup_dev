5882b407e973f91a9fc3f2e7ec647dcb1079b4df
Jay Al-Ansari <j@al-ansari.co.uk>
2022-05-22 19:45:08 +0100

    Attempt to avoid being throttled by pypi, with a simple delay between requests.

--- ubsetup.sh
+++ ubsetup.sh
@@ -873,6 +873,7 @@ read -r -d '' TEXT_BashPythonToolAliases <<- "EOTXT"
 	            newLine="${line/$vers/$ver_found}"
 	            echo "$newLine" >> "$reqsFileTmp"
 	        fi
+            sleep 0.2
 	    done
 	    test -f "$reqsFileTmp" && mv "$reqsFileTmp" "$reqsFile"
 	}
