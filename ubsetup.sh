0cf0c60995cb0dce1a7c0d65c8de0537332b9c7b
Jay Al-Ansari <j@al-ansari.co.uk>
2023-04-03 02:43:07 +0100

    VERSION UPDATES

--- ubsetup.sh
+++ ubsetup.sh
@@ -10,7 +10,7 @@ DebPackages=(
              ["code"]="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64;mscode.deb"
              ["bcompare"]="https://www.scootersoftware.com/bcompare-4.4.6.27483_amd64.deb"
              ["dropbox"]="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb"
-             ["draw.io"]="https://github.com/jgraph/drawio-desktop/releases/download/v20.8.16/drawio-amd64-20.8.16.deb"
+             ["draw.io"]="https://github.com/jgraph/drawio-desktop/releases/download/v21.1.2/drawio-amd64-21.1.2.deb"   # https://github.com/jgraph/drawio-desktop/releases/
              ["mysql-workbench-community"]="https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.32-1ubuntu22.04_amd64.deb"
              ["slack-desktop"]="https://downloads.slack-edge.com/releases/linux/4.29.149/prod/x64/slack-desktop-4.29.149-amd64.deb"
             )
@@ -43,7 +43,7 @@ VeraCryptUrl="https://launchpad.net/veracrypt/trunk/1.25.9/+download/$VeraCryptP
 # DockerComposeUrl="https://github.com/docker/compose/releases/latest/download/docker-compose-Linux-x86_64"
 # DockerComposeUrl="https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64"
 
-TerraformPkg="terraform_1.4.2_linux_amd64.zip"
+TerraformPkg="terraform_1.3.8_linux_amd64.zip"
 TerraformUrl="https://releases.hashicorp.com/terraform/1.4.2/$TerraformPkg"
 
 TflintPkg="tflint_linux_amd64.zip"
@@ -58,7 +58,7 @@ TflintUrl="https://github.com/terraform-linters/tflint/releases/latest/download/
 AwsCliPkg="awscli-exe-linux-x86_64.zip"
 AwsCliUrl="https://awscli.amazonaws.com/$AwsCliPkg"
 
-PlantUmlVer="1.2023.4"
+PlantUmlVer="1.2023.5"
 PlantUmlUrl="https://github.com/plantuml/plantuml/releases/download/v$PlantUmlVer/plantuml-$PlantUmlVer.jar"
 PlantumlTargetBin="/usr/local/bin/plantuml.jar"
 
@@ -66,7 +66,7 @@ AndroidPkg="android-studio-2022.1.1.21-linux.tar.gz"
 AndroidUrl="https://dl.google.com/dl/android/studio/ide-zips/2022.1.1.21/$AndroidPkg"
 AndroidInstallDir="$InstallDir/androidstudio"
 
-FlutterPkg="flutter_linux_3.7.7-stable.tar.xz"
+FlutterPkg="flutter_linux_3.7.9-stable.tar.xz"
 FlutterUrl="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/$FlutterPkg"
 FlutterInstallDir="$InstallDir/flutterdev"
 
