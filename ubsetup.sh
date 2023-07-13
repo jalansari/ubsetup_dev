7401d78ae268e7436e91cb7a7234a31585808f68
Jay Al-Ansari <j@al-ansari.co.uk>
2023-07-13 02:45:43 +0100

    Highlight branch "main".

--- ubsetup.sh
+++ ubsetup.sh
@@ -1031,7 +1031,7 @@ read -r -d '' TEXT_BashGitAliases <<- "EOTXT"
 	function ____git_get_branch() {
 	    branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d; s/^* //;')
 	    if [[ -n $branch ]]; then
-	        if [[ "$branch" == "master" ]]; then
+	        if [[ "$branch" =~ master|main ]]; then
 	            branch_clr="1;31m"
 	        elif [[ "$branch" == "staging" ]]; then
 	            branch_clr="1;33m"
