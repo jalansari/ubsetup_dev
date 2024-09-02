a3f4964b979ea68bde8f03d3ba845344729c1203
Jay Al-Ansari <j@al-ansari.co.uk>
2024-09-02 01:51:26 +0100

    Colour output for cpr commands.

--- ubsetup.sh
+++ ubsetup.sh
@@ -1322,8 +1322,8 @@ read -r -d '' TEXT_BashPythonToolAliases <<- "EOTXT"
 	}
 	export -f ____check_py_requirements____
 	alias cpr='____check_py_requirements____'
-	alias cprall='for reqs in requirements*; do echo $reqs; echo =====; ____check_py_requirements____ $reqs; echo; done'
-	alias cprallr="find -type f -name 'requirements*.txt' | xargs -I {} bash -c 'echo {}; echo =====; ____check_py_requirements____ {}; echo; sleep 5'"
+	alias cprall='for reqs in requirements*; do echo -e "\033[30;1;106m${reqs}\n=====\033[0m"; ____check_py_requirements____ $reqs; echo; done'
+	alias cprallr="find -type f -name 'requirements*.txt' | xargs -I {} bash -c 'echo -e \"\033[30;1;106m{}\n=====\033[0m\"; ____check_py_requirements____ {}; echo; sleep 5'"
 
 	function ____cleanpydir____() {
 	    rm -rf testresults.xml .coverage .cache .pytest_cache .mypy_cache htmlcov .hypothesis .benchmarks *.egg\-info
