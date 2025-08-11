33ffb56215ea66a69e816d0a6d812976552ae1bf
Jay Al-Ansari <j@al-ansari.co.uk>
2025-08-11 01:22:43 +0100

    VERSION UPDATES

--- ubsetup.sh
+++ ubsetup.sh
@@ -9,7 +9,7 @@
 declare -A DebPackages
 DebPackages=(
              ["code"]="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64;mscode.deb"
-             ["codium"]="https://github.com/VSCodium/vscodium/releases/download/1.102.24914/codium_1.102.24914_amd64.deb"
+             ["codium"]="https://github.com/VSCodium/vscodium/releases/download/1.103.05312/codium_1.103.05312_amd64.deb"
              ["bcompare"]="https://www.scootersoftware.com/files/bcompare-5.1.2.31185_amd64.deb"
              ["draw.io"]="https://github.com/jgraph/drawio-desktop/releases/download/v28.0.6/drawio-amd64-28.0.6.deb"
              ["mysql-workbench-community"]="https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.43-1ubuntu24.04_amd64.deb"
@@ -21,21 +21,21 @@ InstallDir="/usr/share"
 UsrLocalDir="/usr/local"
 
 
-NodeJsVer="node-v22.17.1-linux-x64"
+NodeJsVer="node-v22.18.0-linux-x64"
 NodeJsPkg="$NodeJsVer.tar.xz"
-NodeJsUrl="https://nodejs.org/dist/v22.17.1/$NodeJsPkg"
+NodeJsUrl="https://nodejs.org/dist/v22.18.0/$NodeJsPkg"
 NodeInstallDir="$InstallDir/nodejs"
 
 FossilScmPkg="fossil-linux-x64-2.26.tar.gz"
 FossilScmUrl="https://fossil-scm.org/home/uv/$FossilScmPkg"
 FossilInstallDir="$InstallDir/fossilscm"
 
-GoLangPkg="go1.24.5.linux-amd64.tar.gz"
+GoLangPkg="go1.24.6.linux-amd64.tar.gz"
 GoLangUrl="https://go.dev/dl/$GoLangPkg"
 GoPath="$UsrLocalDir/go"
 
-VagrantPkg="vagrant_2.4.7_linux_amd64.zip"
-VagrantUrl="https://releases.hashicorp.com/vagrant/2.4.7/$VagrantPkg"
+VagrantPkg="vagrant_2.4.8_linux_amd64.zip"
+VagrantUrl="https://releases.hashicorp.com/vagrant/2.4.8/$VagrantPkg"
 
 TelegramPackage="telegram_linux.tar.xz"
 TelegramPackageHttpURL="https://telegram.org/dl/desktop/linux"
@@ -75,8 +75,8 @@ PlantUmlVer="1.2025.4"
 PlantUmlUrl="https://github.com/plantuml/plantuml/releases/download/v$PlantUmlVer/plantuml-$PlantUmlVer.jar"
 PlantumlTargetBin="/usr/local/bin/plantuml.jar"
 
-AndroidPkg="android-studio-2025.1.1.14-linux.tar.gz"
-AndroidUrl="https://dl.google.com/dl/android/studio/ide-zips/2025.1.1.14/$AndroidPkg"
+AndroidPkg="android-studio-2025.1.2.11-linux.tar.gz"
+AndroidUrl="https://dl.google.com/dl/android/studio/ide-zips/2025.1.2.11/$AndroidPkg"
 AndroidInstallDir="$InstallDir/androidstudio"
 
 FlutterPkg="flutter_linux_3.32.8-stable.tar.xz"
